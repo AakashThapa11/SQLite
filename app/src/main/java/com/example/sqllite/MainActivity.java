b8204e21fea277c643d53b78efb1bda3b67ed962
Aakash <aakash.thapa19168@gmail.com>
2021-01-15 20:27:43 +0530

    SQLite

---
+++ app/src/main/java/com/example/sqllite/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.sqllite;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout layout;
+    Button login, register;
+    Handler handler = new Handler();
+    Runnable runnable = () -> { layout.setVisibility(View.VISIBLE);};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        layout = (RelativeLayout)findViewById(R.id.reLay);
+        login = findViewById(R.id.loginbttn);
+        register = findViewById(R.id.registerbttn);
+
+        handler.postDelayed(runnable, 3000);
+        
+        
+        login.setOnClickListener((view) ->{
+            Intent i = new Intent(MainActivity.this, loginscreen.class);
+            startActivity(i);
+        });
+
+        register.setOnClickListener((view) ->{
+            Intent i = new Intent(MainActivity.this, register.class);
+            startActivity(i);
+        });
+    }
+}
